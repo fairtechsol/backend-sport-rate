41bf53c269031a6f3cc8225a1ad700a0c38d186d
nain4884 <nain4884@gmail.com>
2025-01-19 18:55:03 +0530

    Add gmid to formatted odds in getGameData function

--- getGameData.js
+++ getGameData.js
@@ -1086,6 +1086,7 @@ function formateOdds(data, additionDetails) {
   return {
     marketId: additionDetails.marketId,
     mid: data?.mid || additionDetails?.marketId,
+    gmid: data.gmid,
     status: data?.status,
     inplay: data?.inplay,
     gtype: additionDetails.gtype || data?.gtype,
