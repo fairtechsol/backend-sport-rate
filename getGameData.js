e1d702459502313294d383ed7490eb9f38df9762
nain4884 <nain4884@gmail.com>
2025-02-07 16:21:44 +0530

    Restore live data emission for cricket and football data while commenting out Redis rate storage

--- getGameData.js
+++ getGameData.js
@@ -420,11 +420,11 @@ async function getCricketData(marketId, matchId) {
     }
   }
 
-  externalRedis.set(matchId + "_expertRate", JSON.stringify(expertResult));
-  externalRedis.set(matchId + "_userRate", JSON.stringify(returnResult));
+  // externalRedis.set(matchId + "_expertRate", JSON.stringify(expertResult));
+  // externalRedis.set(matchId + "_userRate", JSON.stringify(returnResult));
   
-  // io.to(matchId).emit("liveData" + matchId, returnResult);
-  // io.to(matchId + 'expert').emit("liveData" + matchId, expertResult);
+  io.to(matchId).emit("liveData" + matchId, returnResult);
+  io.to(matchId + 'expert').emit("liveData" + matchId, expertResult);
 }
 exports.getCricketData = getCricketData;
 
@@ -603,11 +603,11 @@ async function getFootBallData(marketId, matchId) {
   }
 
   
-  await externalRedis.set(matchId + "_expertRate", JSON.stringify(expertResult));
-  await externalRedis.set(matchId + "_userRate", JSON.stringify(returnResult));
+  // await externalRedis.set(matchId + "_expertRate", JSON.stringify(expertResult));
+  // await externalRedis.set(matchId + "_userRate", JSON.stringify(returnResult));
 
-  // io.to(matchId).emit("liveData" + matchId, returnResult);
-  // io.to(matchId + 'expert').emit("liveData" + matchId, expertResult);
+  io.to(matchId).emit("liveData" + matchId, returnResult);
+  io.to(matchId + 'expert').emit("liveData" + matchId, expertResult);
 }
 exports.getFootBallData = getFootBallData;
 
